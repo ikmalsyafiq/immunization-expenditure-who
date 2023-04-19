46431beebf89b74189ab239a763949365bb6bc37
Ikmal Syafiq Bin Mahadi <ikmal.mahadi@airselangor.com>
2023-04-19 12:01:35 +0800

    change app

--- app.py
+++ app.py
@@ -3,10 +3,20 @@ import pandas as pd
 import plotly.express as px
 import numpy as np
 from scipy import stats
+import pycountry
+
+def get_country_iso_alpha3(country_name):
+    try:
+        return pycountry.countries.lookup(country_name).alpha_3
+    except:
+        return None
 
 # Load the CSV file
 df = pd.read_csv('cleaned_data_with_predictions.csv')
 df["Year"] = pd.to_datetime(df["Year"], format='%Y').dt.strftime('%Y')
+
+# Add ISO country codes to the dataset
+df['iso_alpha'] = df['Country'].apply(get_country_iso_alpha3)
 
 # Set the page title
 st.title('Immunization Expenditure Prediction')
@@ -27,17 +37,6 @@ else:
 
 filtered_df = df[df['Year'].isin(years) & df['Country'].isin(countries) & df['prediction'].isin(predictions)]
 
-# Detect outlier country based on mape column
-df_out = filtered_df.dropna(subset=['mape'])
-z_scores = np.abs(stats.zscore(df_out['mape']))
-threshold = 1
-outliers = df_out[(z_scores > threshold) | (z_scores < -threshold)]
-
-# Display the outlier country if exists
-if not outliers.empty:
-    st.subheader("Outlier Countries")
-    st.dataframe(outliers[["Country", "Year", "mape"]].reset_index(drop=True))
-
 # Allow the user to choose which columns to show in the data table
 default_columns = ['Country', 'Year', 'Immunization USD Mil', 'Immunization USD Mil_predicted', 'mape', 'prediction']
 columns = st.multiselect('Select column(s) to display', options=filtered_df.columns, default=default_columns)
@@ -46,29 +45,93 @@ columns = st.multiselect('Select column(s) to display', options=filtered_df.colu
 st.subheader('Data Table')
 st.dataframe(filtered_df[columns])
 
+# Detect outlier country based on mape column
+df_out = filtered_df.dropna(subset=['mape'])
+z_scores = np.abs(stats.zscore(df_out['mape']))
+threshold = 1
+outliers = df_out[(z_scores > threshold) | (z_scores < -threshold)]
+
+# Display the outlier country if exists
+if not outliers.empty:
+    try:
+        st.subheader("Outlier Countries")
+        st.markdown("""These are the countries that are considered outliers due to their predicted total immunization cost over 1 standard deviation in the Z-score scale.
+        In other words, outliers with Z-scores over 1 standard deviation are values that are much 
+        further away from the typical values in a dataset, and may warrant further investigation to understand why they differ so much.
+        """)
+        st.dataframe(outliers[["Country", "Year", 'Immunization USD Mil', 'Immunization USD Mil_predicted', "mape"]].reset_index(drop=True))
+
+        st.subheader('Choropleth Map for Outlier Countries')
+        value_col_options = filtered_df.columns
+        default_x_column = 'mape'
+        value_col_outliers = st.selectbox('Select a value column', options=filtered_df.columns, index=filtered_df.columns.get_loc(default_x_column))
+        fig_outliers = px.choropleth(outliers, locations='iso_alpha', color=value_col_outliers, hover_name='Country',hover_data=['Immunization USD Mil', 'Immunization USD Mil_predicted', 'mape'], projection='natural earth', title=f'{value_col_outliers} by Outlier Country')
+        st.plotly_chart(fig_outliers)
+    except:
+        mm = True
+
+st.subheader('Self Service Visualization')
+st.markdown("""Choose from Choropleth Map, Scatter Plot, Bar Chart, Line Chart and Box Plot to 
+visualize immunization data.""")
+            
 # Allow the user to choose which type of plot to display
-plot_types = ('Scatter Plot', 'Bar Chart', 'Line Chart', 'Box Plot')
-default_plot_type = 'Scatter Plot'
+plot_types = ('Choropleth Map', 'Scatter Plot', 'Bar Chart', 'Line Chart', 'Box Plot')
+default_plot_type = 'Choropleth Map'
 plot_type = st.selectbox('Select a plot type', options=plot_types, index=plot_types.index(default_plot_type))
 
-# Allow the user to choose which columns to show in the plot
-default_x_column = 'Immunization USD Mil'
-default_y_column = 'Immunization USD Mil_predicted'
-x_column = st.selectbox('Select a column for the x-axis', options=filtered_df.columns, index=filtered_df.columns.get_loc(default_x_column))
-y_column = st.selectbox('Select a column for the y-axis', options=filtered_df.columns, index=filtered_df.columns.get_loc(default_y_column))
-
-# Format the subheader text with the selected column names
-subheader_text = f'{plot_type} ({x_column} vs. {y_column})'
-st.subheader(subheader_text)
 
 # Display the selected type of plot with the selected columns
-if plot_type == 'Scatter Plot':
+if plot_type == 'Choropleth Map':
+    st.subheader('Choropleth Map')
+    value_col_options = filtered_df.columns
+    default_x_column = 'Immunization USD Mil'
+    value_col = st.selectbox('Select a value column',options=filtered_df.columns, index=filtered_df.columns.get_loc(default_x_column))
+    fig = px.choropleth(filtered_df, locations='iso_alpha', color=value_col, hover_name='Country', projection='natural earth', title=f'{value_col} by Country')
+    #st.plotly_chart(fig)
+elif plot_type == 'Scatter Plot':
+    default_x_column = 'Immunization USD Mil'
+    default_y_column = 'Immunization USD Mil_predicted'
+    x_column = st.selectbox('Select a column for the x-axis', options=filtered_df.columns, index=filtered_df.columns.get_loc(default_x_column))
+    y_column = st.selectbox('Select a column for the y-axis', options=filtered_df.columns, index=filtered_df.columns.get_loc(default_y_column))
+
+    # Format the subheader text with the selected column names
+    subheader_text = f'{plot_type} ({x_column} vs. {y_column})'
+    st.subheader(subheader_text)
+
     fig = px.scatter(filtered_df, x=x_column, y=y_column)
+
 elif plot_type == 'Bar Chart':
+    default_x_column = 'Immunization USD Mil'
+    default_y_column = 'Immunization USD Mil_predicted'
+    x_column = st.selectbox('Select a column for the x-axis', options=filtered_df.columns, index=filtered_df.columns.get_loc(default_x_column))
+    y_column = st.selectbox('Select a column for the y-axis', options=filtered_df.columns, index=filtered_df.columns.get_loc(default_y_column))
+
+    # Format the subheader text with the selected column names
+    subheader_text = f'{plot_type} ({x_column} vs. {y_column})'
+    st.subheader(subheader_text)
+
     fig = px.bar(filtered_df, x=x_column, y=y_column)
+
 elif plot_type == 'Line Chart':
+    default_x_column = 'Immunization USD Mil'
+    default_y_column = 'Immunization USD Mil_predicted'
+    x_column = st.selectbox('Select a column for the x-axis', options=filtered_df.columns, index=filtered_df.columns.get_loc(default_x_column))
+    y_column = st.selectbox('Select a column for the y-axis', options=filtered_df.columns, index=filtered_df.columns.get_loc(default_y_column))
+    # Format the subheader text with the selected column names
+    subheader_text = f'{plot_type} ({x_column} vs. {y_column})'
+    st.subheader(subheader_text)
+
     fig = px.line(filtered_df, x=x_column, y=y_column)
-else:
+
+else: # Box Plot
+    default_x_column = 'Immunization USD Mil'
+    default_y_column = 'Immunization USD Mil_predicted'
+    x_column = st.selectbox('Select a column for the x-axis', options=filtered_df.columns, index=filtered_df.columns.get_loc(default_x_column))
+    y_column = st.selectbox('Select a column for the y-axis', options=filtered_df.columns, index=filtered_df.columns.get_loc(default_y_column))
+    # Format the subheader text with the selected column names
+    subheader_text = f'{plot_type} ({x_column} vs. {y_column})'
+    st.subheader(subheader_text)
+
     fig = px.box(filtered_df, x=x_column, y=y_column)
-    
+
 st.plotly_chart(fig)
